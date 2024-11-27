e4360e1dd38eb5131d08cee0348cb2038a42948a
hongky06 <dohongky2006@gmail.com>
2024-11-27 22:41:48 +0700

    Add files via upload

---
+++ ss7 b3.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+	int arr[5]={1,2,3,4,6};
+	if (arr[0] % 2 == 0 ){
+		printf("\n so chan co trong day la %d",arr[0]);
+    }else {
+	}
+	if (arr[1] % 2 == 0 ){
+		printf("\n so chan co trong day la %d",arr[1]);
+	}else{
+	}
+	
+	if (arr[2] % 2 == 0 ){
+		printf("\n so chan co trong day la %d",arr[2]);
+	}else{
+	}
+	 
+	if (arr[3] % 2 == 0 ){
+	printf("\n so chan co trong day la %d",arr[3]);
+	}else{
+	}
+	if (arr[4] % 2 == 0 ){
+		printf("\n so chan co trong day la %d",arr[4]);
+		
+	}else{
+	}
+	 
+	
+	
+	return 0;
+}
